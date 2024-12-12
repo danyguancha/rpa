8565c6b762e97ea36f0a62baf57e629d9456eda3
danyguancha <dany.1701627413@ucaldas.edu.co>
2024-12-12 00:21:30 -0500

    first commit

---
+++ tests/test_prueba.py
@@ -0,0 +1,20 @@
+
+def test_multiplicacion():
+    assert 2 * 2 == 4
+
+def test_suma():
+    assert 2 + 2 == 4
+
+def test_resta():
+    assert 2 - 2 == 0
+
+def test_division():
+    assert 2 / 2 == 1
+
+def test_potencia():
+    assert 2 ** 2 == 4
+
+def test_modulo():
+    assert 2 % 2 == 0
+
+
